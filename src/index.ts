da23b925dd698aad835a7d8b5923d32e37bf9cd2
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-04 16:55:12 +0530

    dummy server for testing the react.js lazy loading frontend

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { TODOS } from "./Todo.js";
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+app.get("/", async (_req: Request, res: Response) => {
+  res.json("Working!").status(200);
+});
+
+app.get("/linkedin", async (_req: Request, res: Response) => {
+  res.json({ network: 7, jobs: 8, messaging: 7, notifications: 5 }).status(200);
+});
+
+app.get("/todo", async (req: Request, res: Response) => {
+  const { id } = req.query;
+
+  res
+    .json(TODOS.find((todo) => todo.id === parseInt(id as string)))
+    .status(200);
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running at", PORT);
+});
